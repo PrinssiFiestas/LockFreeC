3843fc16ec9f49ff90e2b1330ccb77ccbce50d61
Lorenzo Fiestas <hik.lorenzo@hotmail.com>
2024-09-08 20:13:03 +0300

    Added docs to generic macros

--- lfc.h
+++ lfc.h
@@ -10,30 +10,51 @@
 #include <string.h>
 #include <assert.h>
 
+
+// ----------------------------------------------------------------------------
+//
+//          API REFERENCE
+//
+// ----------------------------------------------------------------------------
+
+
 #if ATOMIC_LLONG_LOCK_FREE == 2
+/** Guaranteed lock-free when used as atomic.*/
 typedef unsigned long long LFUint;
 #else
 static_assert(ATOMIC_INT_LOCK_FREE == 2, "No suitable atomics available.");
+/** Guaranteed lock-free when used as atomic.*/
 typedef unsigned LFUint;
 #endif
 
+// ------------------------------------------------------------
+// Wait-free Single Producer Single Consumer type generic queue
+
+/** Generic SPSC queue type.*/
 #define LFQueue(T) T*
 
-#define lf_queue(T,...) (T*)&LF_OVERLOAD2(__VA_ARGS__, LF_QUEUE_WITH_BUFFER, LF_QUEUE_WOUT_BUFFER,)(T,__VA_ARGS__)
+/** Create generic queue. C only.
+ * @p buffer_length specifies how many elements of type @p T fit in @p buffer.
+ * If @p buffer is not provided, it will be created on stack if local scope, or
+ * statically if global scope.
+ */
+#define lf_queue(T,/* void* buffer = static_buffer, size_t buffer_length */...) (T*)&LF_OVERLOAD2(__VA_ARGS__, LF_QUEUE_WITH_BUFFER, LF_QUEUE_WOUT_BUFFER,)(T,__VA_ARGS__)
 
-#if __STDC_VERSION__ >= 202311L
-#define LF_TYPEOF(...) typeof(__VA_ARGS__)
-#else
-#define LF_TYPEOF(...) __typeof__(__VA_ARGS__)
-#endif
+/** Generic enqueue.
+ * @return `true` if @p ELEM got successfully enqueued, `false` if queue was
+ * full, in which case the element was not queued.
+ */
+#define lf_enqueue(/* LFQueue(T) */QUEUE,/* T */ELEM) lf_spsc_enqueue((LFSPSCQueue*)(QUEUE), &(struct { LF_TYPEOF(*(QUEUE))_; }) { ELEM }, sizeof(ELEM))
 
-#define lf_enqueue(QUEUE, ELEM) lf_spsc_enqueue((LFSPSCQueue*)(QUEUE), \
-    &(struct { LF_TYPEOF(*(QUEUE))_; }) { ELEM }, sizeof(ELEM))
+/** Generic dequeue.
+ * @return @p out if @p queue was not empty, `NULL` otherwise. If @p out is not
+ * provided, the returned element will be allocated on stack.
+ */
+#define lf_dequeue(/* LFQueue(T) queue, T* out = static_buffer */...) LF_OVERLOAD2(__VA_ARGS__, LF_DEQUEUE_WITH_BUFFER, LF_DEQUEUE_WOUT_BUFFER,)(__VA_ARGS__)
 
-#define LF_OVERLOAD2(_0, _1, RESOLVED, ...) RESOLVED
-
-#define LF_QUEUE_WOUT_BUFFER(T, BUFFER_LENGTH)         (LFSPSCQueue){.buffer = (T[BUFFER_LENGTH]){0}, .buffer_length = (BUFFER_LENGTH) }
-#define LF_QUEUE_WITH_BUFFER(T, BUFFER, BUFFER_LENGTH) (LFSPSCQueue){.buffer = (BUFFER),              .buffer_length = (BUFFER_LENGTH) }
+// ------------------------------------------------------------
+// Wait-free Single Producer Single Consumer non-generic queue
+// TODO document these too.
 
 #if !__cplusplus
 #define LF_RESTRICT restrict
@@ -71,11 +92,6 @@ static inline bool lf_spsc_enqueue(LFSPSCQueue* queue, const void*LF_RESTRICT da
     return true;
 }
 
-#define lf_dequeue(...) LF_OVERLOAD2(__VA_ARGS__, LF_DEQUEUE_WITH_BUFFER, LF_DEQUEUE_WOUT_BUFFER,)(__VA_ARGS__)
-#define LF_DEQUEUE_WOUT_BUFFER(QUEUE) \
-    (LF_TYPEOF(QUEUE))lf_spsc_dequeue((LFSPSCQueue*)(QUEUE), &(LF_TYPEOF(*(QUEUE))){0}, sizeof(*(QUEUE)))
-#define LF_DEQUEUE_WITH_BUFFER(QUEUE, BUFFER) (LF_TYPEOF(QUEUE))lf_spsc_dequeue((LFSPSCQueue*)(QUEUE), (BUFFER), sizeof(*(QUEUE) = (BUFFER)))
-
 static inline void* lf_spsc_dequeue(LFSPSCQueue* queue, void*LF_RESTRICT out, size_t out_size)
 {
     LFUint old_tail = atomic_load_explicit(&queue->tail, memory_order_acquire);
@@ -88,5 +104,35 @@ static inline void* lf_spsc_dequeue(LFSPSCQueue* queue, void*LF_RESTRICT out, si
     memcpy(out, (char*)queue->buffer + out_size * lf_index(old_tail, queue->buffer_length), out_size);
     return out;
 }
+
+
+// ----------------------------------------------------------------------------
+//
+//          END OF API REFERENCE
+//
+//          Code below is for internal usage and may change without notice.
+//
+// ----------------------------------------------------------------------------
+
+
+#if __STDC_VERSION__ >= 202311L
+#define LF_TYPEOF(...) typeof(__VA_ARGS__)
+#elif __cplusplus
+#define LF_TYPEOF(...) decltype(__VA_ARGS__)
+#else
+#define LF_TYPEOF(...) __typeof__(__VA_ARGS__)
+#endif
+
+#define LF_OVERLOAD2(_0, _1, RESOLVED, ...) RESOLVED
+
+#define LF_QUEUE_WOUT_BUFFER(T, BUFFER_LENGTH) \
+    (LFSPSCQueue){.buffer = (T[BUFFER_LENGTH]){0}, .buffer_length = (BUFFER_LENGTH) }
+#define LF_QUEUE_WITH_BUFFER(T, BUFFER, BUFFER_LENGTH) \
+    (LFSPSCQueue){.buffer = (BUFFER), .buffer_length = (BUFFER_LENGTH) }
+
+#define LF_DEQUEUE_WOUT_BUFFER(QUEUE) \
+    (LF_TYPEOF(QUEUE))lf_spsc_dequeue((LFSPSCQueue*)(QUEUE), &(LF_TYPEOF(*(QUEUE))){0}, sizeof(*(QUEUE)))
+#define LF_DEQUEUE_WITH_BUFFER(QUEUE, BUFFER) \
+    (LF_TYPEOF(QUEUE))lf_spsc_dequeue((LFSPSCQueue*)(QUEUE), (BUFFER), sizeof(*(QUEUE) = (BUFFER)))
 
 #endif // LFC_H_INCLUDED
